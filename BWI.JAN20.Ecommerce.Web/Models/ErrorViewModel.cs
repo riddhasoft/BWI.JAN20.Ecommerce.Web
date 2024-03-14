bc450e10297b7c436e45e1a289afd39308a41b92
Binod sapkota <binodsapkota96@gmail.com>
2024-03-14 18:30:02 +0545

    Add project files.

---
+++ BWI.JAN20.Ecommerce.Web/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace BWI.JAN20.Ecommerce.Web.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
