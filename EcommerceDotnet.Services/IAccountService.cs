92260ede8db9246c4e9f6f2e237e08e060a7d7a0
Binod sapkota <binodsapkota96@gmail.com>
2024-03-18 18:26:29 +0545

    user model and interface

---
+++ EcommerceDotnet.Services/IAccountService.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EcommerceDotnet.Services
+{
+    public interface IAccountService
+    {
+        List<UserModel> List();
+        int Add(UserModel user);
+        int Update(UserModel user);
+        int Delete(int id);
+        UserModel Find(int id);
+    }
+}
